dde8030be59667b8d35901f1ef5d8e2a6c5a207d
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2024-03-25 11:47:04 -0300

    adds mock buttons

--- www/js/index.js
+++ www/js/index.js
@@ -21,19 +21,36 @@
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
+const API_URL = 'https://pedidos-pizzaria.glitch.me/admin/pizzas/guiga-dev'
+
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
 
+    // Switch screens buttons setup
     document.querySelectorAll(".btn-switch-screen")
         .forEach(btnSwitchScreen => btnSwitchScreen.addEventListener("click", changeScreen))
+
+    document.getElementById("save-pizza").addEventListener("click", savePizza)
+    document.getElementById("take-picture").addEventListener("click", takePicture)
 }
 
 function changeScreen(btn) {
     let { nextScreen, originScreen } = btn.srcElement.dataset
-    console.log({ nextScreen, originScreen })
 
     document.getElementById(originScreen).classList.add("hidden")
     document.getElementById(nextScreen).classList.remove("hidden")
+}
+
+function savePizza() {
+    console.log("SAVING PIZZA")
+}
+
+function takePicture() {
+    console.log("TAKING PICTURE")
+}
+
+function deletePizza() {
+    console.log("DELETING PIZZA")
 }
